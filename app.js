455627432ec13bfc2bda7c221b0b27de942cf57a
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2022-12-13 19:23:26 +0530

    Fix: Bug Fix

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var app = express();
 require('./src/db/connection');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.use(cors());
 
